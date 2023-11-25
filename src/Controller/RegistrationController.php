565a394d662905e505aad840d5856883bac034cb
MPXH <98483069+MPXH@users.noreply.github.com>
2023-11-25 16:21:52 +0100

    cryptage mdp

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -8,10 +8,19 @@ use Doctrine\Persistence\ManagerRegistry as PersistenceManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RegistrationController extends AbstractController
 {
+    private $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
+
     #[Route(path: '/registration', name: 'register')]
     public function register(Request $request, PersistenceManagerRegistry $doctrine): Response
     {
@@ -20,9 +29,14 @@ class RegistrationController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword(
+                $this->passwordHasher->hashPassword($user, $form->get("password")->getData())
+            );
             $em = $doctrine->getManager();
             $em->persist($user);
             $em->flush();
+
+            $this->addFlash("success", "Vous êtes inscris!");
         }
 
         return $this->render('registration/register.html.twig', [
