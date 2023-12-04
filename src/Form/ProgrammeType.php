d45ea9420ba4d90aed9aa587e1cb28c988c3a6d7
corentinhoizey <151189435+corentinhoizey@users.noreply.github.com>
2023-12-04 17:43:36 +0100

    CRUD PROGRAMME

---
+++ src/Form/ProgrammeType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Programme;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProgrammeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('libelle')
+            ->add('description')
+            ->add('type')
+            ->add('createur')
+            ->add('estFavori')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Programme::class,
+        ]);
+    }
+}
