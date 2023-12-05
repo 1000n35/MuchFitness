be109f752dc0053ff15b0f605d9e9ef53ec05e08
MPXH <98483069+MPXH@users.noreply.github.com>
2023-12-05 15:08:14 +0100

    Modif Home
    Creation VRAIE page home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,12 +8,9 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
-    public function index(): Response
-    {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+    #[Route('/', name: 'home')]
+    public function home() {
+        return $this->render('home/index.html.twig');
     }
 }
 
