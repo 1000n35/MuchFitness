b1a5bf1db1e3725e9ab509ce729392e3a9985f64
brindille987 <60676785+brindille987@users.noreply.github.com>
2023-12-07 15:01:42 +0100

    Résolutions d'erreurs sur le formulaire

--- src/Form/RegistrationType.php
+++ src/Form/RegistrationType.php
@@ -24,7 +24,7 @@ class RegistrationType extends AbstractType
                 'format' => 'yyyy-MM-dd',
             ])
             ->add('telephone')
-            ->add('isCoach')
+            ->add('coach')
         ;
     }
 
