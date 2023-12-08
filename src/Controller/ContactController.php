e007d21d7856110bcd9c1af0073d0dd5e20ba279
MPXH <98483069+MPXH@users.noreply.github.com>
2023-12-08 18:13:09 +0100

    Autocompletion Contact Coach
    Ajoute automatiquement le nom/prénom utilisateur connecté et le mail du coach qui a créé le programme.

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Contact;
+use App\Entity\Programme;
 use App\Form\ContactType;
 use Doctrine\Persistence\ManagerRegistry as PersistenceManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -22,6 +24,38 @@ class ContactController extends AbstractController
             $contact->setNom($this->getUser()->getNom());
             $contact->setPrenom($this->getUser()->getPrenom());
             $contact->setEmail("admin@gmail.com");
+        }
+
+        $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contact = $form->getData();
+
+            $em = $doctrine->getManager();
+            $em->persist($contact);
+            $em->flush();
+
+            $this->addFlash("success", "Mail envoyé!");
+
+            return $this->redirect("contact");
+        }
+
+        return $this->render('contact/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/contact/{id}', name: 'contact_coach')]
+    public function contactCoach(Programme $programme, Request $request, PersistenceManagerRegistry $doctrine, Security $security): Response
+    {
+        $contact = new Contact();
+        $user = $security->getUser();
+
+        if($this->getUser()) {
+            $contact->setNom($user->getNom());
+            $contact->setPrenom($user->getPrenom());
+            $contact->setEmail($programme->getCreateur()->getEmail());
         }
 
         $form = $this->createForm(ContactType::class, $contact);
